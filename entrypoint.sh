64f1dfb434bd509de81e3119029eb2c8bfbd34b5
Sourabh Shirhatti <shirhatti@outlook.com>
2019-10-03 02:12:31 -0700

    Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 dotnet publish -r win-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
-dotnet publish -r win-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
+dotnet publish -r osx-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
 dotnet publish -r linux-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
